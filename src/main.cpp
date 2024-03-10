1e7bf2761463f463f2eb4b8230280942daff5791
Andrey <kezin636@gmail.com>
2024-03-10 18:25:13 +0300

    final boss

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,6 @@ using namespace geode::prelude;
 #include <Geode/modify/EndLevelLayer.hpp>
 
 
-class CallbackActions : public CCObject {
-public:
-
-	void showDelayedAlert() {
-		FLAlertLayer::create(
-			"Ooops!", 
-			"Seems like you <cr>have no NodeIDs installed.</c> Consider installing it via <cy>Geode Mod Index.</c>", 
-			"OK"
-		)->show();
-	}
-};
-
-
 class $modify(CustomEndLevelLayer, EndLevelLayer) {
 
 	void customSetup() {
@@ -25,8 +12,8 @@ class $modify(CustomEndLevelLayer, EndLevelLayer) {
 		auto value = Mod::get()->getSettingValue<std::string>("static-text");
 		auto text_scale = Mod::get()->getSettingValue<double>("static-text-scale");
 
-		TextArea* complete_message = static_cast<TextArea*>(getChildByIDRecursive("complete-message"));
-		CCLabelBMFont* end_text = static_cast<CCLabelBMFont*>(getChildByIDRecursive("end-text"));
+		auto complete_message = static_cast<TextArea*>(getChildByIDRecursive("complete-message"));
+		auto end_text = static_cast<CCLabelBMFont*>(getChildByIDRecursive("end-text"));
 
 		if (complete_message != nullptr) {
 			complete_message->setString(gd::string(value.c_str()));
@@ -38,16 +25,5 @@ class $modify(CustomEndLevelLayer, EndLevelLayer) {
 			end_text->setScale(text_scale);
 			return;
 		}
-
-
-		auto action = CCCallFunc::create(this, callfunc_selector(CallbackActions::showDelayedAlert));
-		CCArray* actions_array = CCArray::create();
-
-		actions_array->addObject(CCDelayTime::create(0.5f));
-		actions_array->addObject(action);
-
-		// runAction(
-		// 	CCSequence::create(actions_array)
-		// );
 	}
 };
